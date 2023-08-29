ff87c2b3534276eecca72adbe562f7ed87bc2d51
ibnabii <noone@nowhere.com>
2023-08-29 16:18:33 +0200

    Allauth bypass interim page

--- tacom/contest/tests/__init__.py
+++ tacom/contest/tests/__init__.py
@@ -1,2 +1,4 @@
-from contest.tests.models import *
-from contest.tests.basic_views import *
+# from contest.tests.models import *
+from .models import *
+# from contest.tests.basic_views import *
+from .basic_views import *
