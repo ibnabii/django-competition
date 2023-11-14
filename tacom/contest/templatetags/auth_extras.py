24f62d4025f337b4cee4624a7ca8ccf90199f016
ibnabii <someone@somewhere.net>
2023-11-14 17:48:16 +0100

    poc: extra menu item for competition staff

--- tacom/contest/templatetags/auth_extras.py
+++ tacom/contest/templatetags/auth_extras.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='is_translator')
 def is_translator(user):
     return user.groups.filter(name='translators').exists()
+
+
+@register.filter(name='is_contest_staff')
+def is_contest_staff(user):
+    return user.groups.filter(name='contest_staff').exists()
