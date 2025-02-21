cc6c758569530975768266635bf98c7f4f5d7af4
ibnabii <someone@somewhere.net>
2025-02-21 20:26:21 +0100

    fix: old migration (007) failed on new instance due to the issue with default, fixed this now

--- tacom/contest/models.py
+++ tacom/contest/models.py
@@ -5,7 +5,7 @@ from uuid import uuid1
 from random import choices
 from string import ascii_uppercase, digits
 
-from django.db import models
+from django.db import models, ProgrammingError, OperationalError
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
@@ -504,9 +504,11 @@ class Category(models.Model):
 
 
 def code_generator():
-    # for migration only:
-    # return 0
-    maximum_code = Entry.objects.aggregate(models.Max("code"))["code__max"]
+    try:
+        maximum_code = Entry.objects.aggregate(models.Max("code"))["code__max"]
+    except (OperationalError, ProgrammingError):
+        # If the table (or column) doesn't exist yet, return 0
+        return 0
     if maximum_code:
         return int(maximum_code) + 1
     return 1000
