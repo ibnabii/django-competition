6a7ddfd7ba302aa638a770397f4d5d7f40bc1569
ibnabii <someone@somewhere.net>
2025-02-25 22:00:41 +0100

    fix: db backuper

--- tacom/backup_database.py
+++ tacom/backup_database.py
@@ -1,6 +1,7 @@
 import environ
 import gzip
 import subprocess
+import os
 from datetime import datetime
 
 env = environ.Env()
@@ -20,15 +21,23 @@ cmd = [
     str(db_config["PORT"]),
     "-d",
     db_config["NAME"],
-    db_config["OPTIONS"]["options"],
+    "-n",
+    db_config["OPTIONS"]["options"].split("=")[1],
 ]
+
+os.environ["PGPASSWORD"] = db_config["PASSWORD"]
 
 
 with gzip.open(f"{backup_dir}/backup_{timestamp_str}.gz", "wb") as f:
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    popen = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
+    )
 
+    # Iterate over stdout until the process finishes
     for stdout_line in iter(popen.stdout.readline, ""):
-        f.write(stdout_line.encode("utf-8"))
+        if stdout_line:
+            f.write(stdout_line.encode("utf-8"))
 
-        popen.stdout.close()
-        popen.wait()
+    # Close the stdout after reading
+    popen.stdout.close()
+    popen.wait()  # Ensure the process finishes
